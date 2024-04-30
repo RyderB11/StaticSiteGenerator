dac8a205693e15afcc69c70bd09c2a995d23a709
RyderB11 <ryderbrock@ymail.com>
2024-04-30 07:15:30 -0700

    got some working tests as well as working main and textnode.py, ch2 lesson 3

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+from textnode import TextNode
+def main():
+    # These are your dummy values
+    text = "This is a text node"
+    text_type = "bold"  # It looks like you named the variable 'bold', but 'text_type' might be more descriptive
+    url = "https://boot.dev"
+    # here is a fake node to text with our dummy values to make sure its working.
+    node = TextNode(text, text_type, url)
+
+    print(node)
+# not sure wht main is named like this, will look it up
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,2 +1,18 @@
+# this is the class for TextNode, nothing else to add here, we defaulty url to None so if nothing is passed in, None will return, if something is passed in, self.url will match the input
 class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+# here we're trying to create an eq method that should be true if *all* of 2 TextNodes are equal. SO i tried with 2 and statements. will see what happens
+
+    def __eq__(self, other):
+        if isinstance(other,TextNode):
+            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return False
     
+# lastly this is the repr for the function. i have my doubts as this returns the values, but not explicitly printing them, will trial and error this one and if we dont see anything printed, i know where to look.
+    # i was wrong, this needs to be a return and not a print()
+    def __repr__(self):
+        return(f"TextNode({self.text}, {self.text_type}, {self.url})")
